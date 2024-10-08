2f0d45de8a7246ac39976129c3348c8227a6de63
Saroj Kanta Behera <94886665+imsarojkantabehera@users.noreply.github.com>
2024-10-08 14:29:53 +0530

    Update Geocoding Particulate Matter Data.R

--- Geocoding Particulate Matter Data.R
+++ Geocoding Particulate Matter Data.R
@@ -11,7 +11,7 @@
 }
 
 # Read the GPX file (make sure to specify the correct layer)
-GPS <- st_read("GPS.gpx", layer = "track_points")
+GPS <- st_read("GPS_Data.gpx", layer = "track_points")
 
 # Extract and mutate to add latitude and longitude to gpx_data
 GPS <- GPS %>%
@@ -31,13 +31,13 @@ GPS <- GPS %>%
 
 
 # Read the file, skipping the first 36 rows
-DTT <- read.csv("DTT.csv", skip = 36) %>% 
+PM <- read.csv("PM.csv", skip = 36) %>% 
   dplyr::rename(Elapsed_Time = 1, PM1 = 2, PM2.5 = 3, PM4 = 4, PM10 = 5, Total = 6)
-DTT <- subset(DTT, select = -c(7, 8))
+PM <- subset(PM, select = -c(7, 8))
 
 # Extract "Test Start Time" (assuming it's in HH:MM:SS format)
 
-TST <- read.csv("DTT.csv")
+TST <- read.csv("PM.csv")
 
 # Get the Test Start Time from the 7th row and 2nd column
 test_start_time <- TST[6, 2]
@@ -48,11 +48,11 @@ num_time <- as.numeric(time_parts[1]) * 3600 + as.numeric(time_parts[2]) * 60 + 
 
 # Add a new column "Num_Time" where the first value is "num_time"
 # Then add (Elapsed Time - 1) to all rows
-DTT <- DTT %>%
+PM <- PM %>%
   mutate(num_time = num_time + Elapsed_Time - 1)
 
-# Assuming DTT and GPS are your data frames and 'num_time' is the column to match
-merged_data <- merge(DTT, GPS, by = "num_time", all = TRUE)
+# Assuming PM and GPS are your data frames and 'num_time' is the column to match
+merged_data <- merge(PM, GPS, by = "num_time", all = TRUE)
 
 # Select only the specified columns
 merged_data_subset <- merged_data[, c("num_time", "date", "hour", "PM1", "PM2.5", 
@@ -61,4 +61,4 @@ merged_data_subset <- merged_data[, c("num_time", "date", "hour", "PM1", "PM2.5"
 Final_Merged_Data <- na.omit(merged_data_subset)
 Final_Merged_Data$geometry <- NULL  # Remove geometry column
 Final_Merged_Data <- as.data.frame(Final_Merged_Data)
-write.csv(Final_Merged_Data, "PM.csv", row.names = FALSE)
+write.csv(Final_Merged_Data, "Geocoded_PM_Data.csv", row.names = FALSE)
